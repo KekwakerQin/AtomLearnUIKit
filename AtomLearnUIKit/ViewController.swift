4cd96ae119d317231501e3214407f312f24dcf88
Chingis <Kekwaker@gmail.com>
2025-04-08 10:47:47 +0800

    Initial Commit

---
+++ AtomLearnUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AtomLearnUIKit
+//
+//  Created by Qin Chingis on 4/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
