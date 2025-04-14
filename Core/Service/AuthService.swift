d6156844491383e87e0d204e383809e5c670f292
Chingis <Kekwaker@gmail.com>
2025-04-14 16:12:23 +0800

    Abstract AuthService

--- Core/Service/AuthService.swift
+++ Core/Service/AuthService.swift
@@ -1,16 +1,69 @@
 import Foundation
 import FirebaseAuth
-import FirebaseFirestore
 
-final class AuthService {
-    static let shared = AuthService()
-    private init {}
+protocol AuthServiceProtocol {
+    var currentUserID: String? { get }
+    var isAuthorized: Bool { get }
     
-    // MARK: - API
-    
-    
+    func signIn(email: String, password: String, completion: @escaping (Result<Void, Error) -> Void)
+    func register(email: String, password: String, completion: @escaping (Result<Void, Error) -> Void)
+    func signOut()
+}
+
+final class FirebaseAuthService: AuthServiceProtocol {
+    static let shared = FirebaseAuthService()
+    private init() {}
     
     var currentUserID: String? {
-        return Auth.auth().currentUser?.uid
+        Auth.auth().currentUser?.uid
+    }
+    
+    var isAuthorized: Bool {
+        currentUserID != nil
+    }
+    
+    func signIn(email: String, password: String, completion: @escaping (Result<Void,Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else {
+                completion(.success(()))
+            }
+        }
+    }
+    
+    func register(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else {
+                completion(.success(()))
+            }
+        }
+    }
+    
+    func signOut() {
+        try? Auth.auth().signOut()
+    }
+}
+
+final class MockAuthService: AuthServiceProtocol {
+    var currentUserID: String? = nil
+    var isAuthorized: Bool { currentUserID != nil }
+    
+    func signIn(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        currentUserID = "mock-data"
+        completion(.success(()))
+    }
+    
+    func register(email: String, password: String, completion: @escaping (Result<Void, Error) -> Void) {
+        currentUserID = "mock-data"
+        completion(.success(()))
+    }
+    
+    func signOut() {
+        currentUserID = nil
     }
 }
