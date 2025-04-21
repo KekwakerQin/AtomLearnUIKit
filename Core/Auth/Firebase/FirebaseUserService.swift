73d213cb8cb2c213b93b61b6686aac057c14c314
Chingis <Kekwaker@gmail.com>
2025-04-21 11:52:04 +0800

    auth service update

--- Core/Auth/Firebase/FirebaseUserService.swift
+++ Core/Auth/Firebase/FirebaseUserService.swift
@@ -7,8 +7,7 @@ final class FirestoreUserService: UserServiceProtocol {
     private let db = Firestore.firestore()
 
     func createUserIfNeeded(from firebaseUser: User) -> Single<AppUser> {
-        let docRef = db.collection("users").document(firebaseUser.uid)
-        
+        let docRef = FirestorePaths.userRef(uid: firebaseUser.uid)
         return Single.create { single in
             docRef.getDocument { snapshot, error in
                 if let snapshot = snapshot, snapshot.exists {
@@ -40,6 +39,50 @@ final class FirestoreUserService: UserServiceProtocol {
                 }
             }
 
+            return Disposables.create()
+        }
+    }
+    
+    func fetchUser(uid: String) -> Single<AppUser> {
+        let ref = FirestorePaths.userRef(uid: uid)
+        
+        return Single.create { single in
+            ref.getDocument() { snapshot, error in
+                if let error = error {
+                    single(.failure(error))
+                } else if let snapshot = snapshot {
+                    do {
+                        let user = try snapshot.data(as: AppUser.self)
+                        single(.success(user))
+                    } catch {
+                        single(.failure(error))
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func updateUser(_ user: AppUser) -> Completable {
+        guard let uid = user.id else {
+            return .error(NSError(domain: "FirestoreUserService", code: -1, userInfo: [NSLocalizedDescriptionKey: "Missing user id"]))
+        }
+        
+        let ref = FirestorePaths.userRef(uid: uid)
+        
+        return Completable.create { completable in
+            do {
+                try ref.setData(from: user, merge: true) { error in
+                    if let error = error {
+                        completable(.error(error))
+                    } else {
+                        completable(.completed)
+                    }
+                }
+            } catch {
+                completable(.error(error))
+            }
+            
             return Disposables.create()
         }
     }
